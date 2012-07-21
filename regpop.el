6c0a883a8eb479cf9294aa7559c66b2b7d9bbcd0
Jason Filsinger <jason.filsinger@gmail.com>
2012-07-20 22:43:16 -0700

    Major refactoring and cleanup
    + now jumps to start of the regexp sub-expression rather than the start of the line.
    + a mark is pushed before jumping if 'regpop-push-mark-on-jump' is true.
    - removing deprecated functions.

--- regpop.el
+++ regpop.el
@@ -49,90 +49,81 @@ line number containing the regex."
   :type 'boolean
   :group 'regpop)
 
-(defun regpop-goto-line (line)
-  "because goto-line isnt supposed to be used in lisp code."
-  (interactive)
-  (goto-char (point-min))
-  (forward-line (1- line)))
+(defcustom regpop-push-mark-on-jump t
+  "When value is t, the regpop will push a new mark before jumping to the matching entry."
+  :type 'boolean
+  :group 'regpop)
 
-(defun regpop-buffer-lines-to-list(&optional buffer)
-  "Returns a list of strings for every line in a buffer."
-  (let ((prevBuffer (current-buffer))
-	(needBufferSwitch (and buffer (not (eq buffer (current-buffer))))) buffer-lines)
-    (when needBufferSwitch (set-buffer buffer))
-    (setq buffer-lines (split-string (buffer-substring-no-properties (buffer-end -1) (buffer-end 1)) "\n" nil))
-    (when needBufferSwitch (set-buffer prevBuffer))
-    buffer-lines))
 
 (defun regpop-delete-surrounding-whitespace (string)
   "Deletes the leading and trailing whitespace in a string."
   (replace-regexp-in-string "^[[:space:]]?+" "" (replace-regexp-in-string "[[:space:]]?+$" "" string)))
 
-(defun regpop-format-prefix (functionName lineNumber)
-  "Format the function name based on the regpop options"
-  (let ((retStr (when regpop-display-line (number-to-string lineNumber))))
-    (when (and regpop-display-containing-function functionName) (setq retStr (if retStr (concat retStr ":" functionName) functionName)))
-    (when (and retStr (> (length retStr) 0)) (concat "[" retStr "] "))))
+(defun regpop-make-popup-item (function start text)
+  "Make a popup menu item for regpop."
+  (let ((item-function (if regpop-display-containing-function function nil))
+		(item-line (if regpop-display-line (number-to-string (line-number-at-pos start) ) nil)))
+	(let ((item-prefix
+		   (or (and (and (and item-function (stringp item-function)) item-line)
+					(concat item-function ":" item-line)) (or (when (boundp item-function)item-function) (when item-line item-line)))))
+	  (popup-make-item
+	   (concat (or (when item-prefix (concat "[" item-prefix "] ")) "") (regpop-delete-surrounding-whitespace text))
+	   :value start
+	   ))))
 
-(defun regpop-get-match-list (regexToMatch &optional index buffer)
+
+(defun regpop-get-match-list (regex &optional subexp buffer)
   "Return a list of popup-items that match the regex."
-  (let ((retList (list))
-	(buffer-lines (regpop-buffer-lines-to-list (or buffer (current-buffer))))
-	(currLineNumber 0)
-	(initialLine (line-number-at-pos nil))
-	(prevBuffer (current-buffer))
-	(needBufferSwitch (and buffer (not (eq buffer (current-buffer))))))
-    (when needBufferSwitch (set-buffer buffer))
-    (loop for currLine in buffer-lines do
-	  (setq currLineNumber (+ currLineNumber 1))
-	  (when (and currLine (string-match regexToMatch currLine))
-	    (let ((newEntry (or (match-string (if index index 1) currLine) (regpop-delete-surrounding-whitespace currLine))))
-	      (when newEntry
-		(progn
-		  (regpop-goto-line currLineNumber)
-		  (let ((currPrefix (regpop-format-prefix (if regpop-display-containing-function (which-function) nil) currLineNumber)))
-		    (when currPrefix (setq newEntry (format "%s%s" currPrefix newEntry))))
-		  (setq retList (append retList (list (popup-make-item newEntry :value currLineNumber)))))))))
-    (when (> (length retList) 1) (regpop-goto-line initialLine))
-    (when needBufferSwitch (set-buffer prevBuffer))
-    (if (> (length retList) 0) retList nil)))
+  (let ((regexp-subexp (if subexp subexp 0))
+		ret-list)
+	(with-current-buffer (if buffer buffer (current-buffer))
+	  (save-excursion
+		(goto-char (point-max))
+		(while (re-search-backward regex nil t)
+		  (add-to-list 'ret-list (regpop-make-popup-item
+								  (if (featurep 'which-func) (which-function) nil)
+								  (match-beginning regexp-subexp)
+								  (match-string-no-properties regexp-subexp))))))
+	ret-list))
 
-(defun* regpop* (regex &key index buffer point)
+
+(defun* regpop* (regex &key subexp buffer point)
   "Display a popup for all instinces of a regex in a buffer."
-  (let ((regexList (regpop-get-match-list regex index buffer)) tempLine)
-    (when regexList
-      (setq tempLine (if (> (length regexList) 1)
-			 (popup-menu* regexList :isearch regpop-isearch :point point)
-		       (car (popup-item-value regexList))))
-      (when tempLine
-	(progn
-	  (when buffer (set-buffer buffer))
-	  (regpop-goto-line tempLine)
-	  (recenter))))))
+  (let ((regpop-list (regpop-get-match-list regex subexp buffer)) item-point)
+    (when regpop-list
+      (let ((item-point (if (> (length regpop-list) 1)
+							(popup-menu* regpop-list :isearch regpop-isearch :point point)
+						  (popup-item-value (car regpop-list) ))))
+		(when item-point
+		  (progn
+			(when buffer (set-buffer buffer))
+			(when regpop-push-mark-on-jump (push-mark))
+			(goto-char item-point)
+			(recenter)))))))
 
 ;;;###autoload
-(defun regpop (regex &optional index buffer)
+(defun regpop (regex &optional subexp buffer)
   "Display a popup for all instinces of a regex in a buffer."
   (interactive (list (read-from-minibuffer "Regex: ")
-		     (string-to-number (read-from-minibuffer "Regex group index: " nil nil nil nil "0"))))
-  (regpop* regex :index index :buffer buffer))
+					 (string-to-number (read-from-minibuffer "Regex group subexp: " nil nil nil nil "0"))))
+  (regpop* regex :subexp subexp :buffer buffer))
 
 ;;;###autoload
 (defun regpop-todo ()
   "popup all todos in the current buffer."
   (interactive)
-  (regpop* regpop-todo :index 1))
+  (regpop* regpop-todo :subexp 1))
 
 ;;;###autoload
 (defun regpop-stub ()
   "popup all stubs in the current buffer."
   (interactive)
-  (regpop* regpop-stub :index 1))
+  (regpop* regpop-stub :subexp 1))
 
 ;;;###autoload
 (defun regpop-assert ()
   "popup all stubs in the current buffer."
   (interactive)
-  (regpop* regpop-assert :index 1))
+  (regpop* regpop-assert :subexp 1))
 
 (provide 'regpop)
